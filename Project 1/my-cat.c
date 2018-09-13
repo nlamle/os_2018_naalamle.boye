dea468cfadcfc44a07294977e2869dabe8ff21e8
nlamle <naalamle.boye@ashesi.edu.gh>
2018-09-13 00:01:56 +0000

    new files
    new file my-cat.c
    new file my-grep.c
    deleted a.out, main.c, prime.js

---
+++ Project 1/my-cat.c
@@ -0,0 +1,7 @@
+//load the file
+
+//for each line in the file, store in a data structure
+
+//for each item in the data structure, print to console
+
+//close file, clear processor resources
---
+++ Project 1/my-grep.c
@@ -0,0 +1,11 @@
+//load the file, check and handle errors
+
+//take parameters for search term
+
+//for each word in the file, look for search term
+
+//if term exists, add entire line to data structure
+
+//for each item in the data structure, print to console
+
+//close file, clear processor resources

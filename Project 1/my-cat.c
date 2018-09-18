e2c2f3b24372be39ef5d9964cb09f47f645788fe
nlamle <naalamle.boye@ashesi.edu.gh>
2018-09-18 22:27:08 +0000

    added new function
    new function myCat takes parameter of filename, finds number of lines, gets each line and prints.
    
    function main() runs myCat on each file specified in command line

--- Project 1/my-cat.c
+++ Project 1/my-cat.c
@@ -1,7 +1,79 @@
-//load the file
+#include <stdio.h>
+#include <stdlib.h>
 
-//for each line in the file, store in a data structure
+int myCat(char *filename);
 
-//for each item in the data structure, print to console
+int main(int argc, char *argv[])
+{
 
+	//if no file is specified in command line
+	if (argc == 1){
+		exit(0);
+	}
+
+	//if multiple files
+	else{
+		//files printed out until end of file list
+		int i = 1;
+		while (i < argc){
+			myCat(argv[i]);
+			//printf("%s\n", argv[i]);
+			i = i + 1;
+		}
+	}
+
+	return 0;
+}
+
+int myCat(char *filename){
+	//open file for reading
+	FILE *fptr = fopen(filename, "r");
+
+
+	//get number of lines in the file
+	int nlines = 0;
+	char chrs;
+	chrs = fgetc(fptr);
+
+	while (chrs != EOF){
+		if (chrs == '\n')
+		{
+			nlines = nlines + 1;
+		}
+		
+		chrs = fgetc(fptr);
+	}
+
+
+
+	printf("There are %d lines in the file '%s'.\n\n", nlines, filename);
+	
+	char lines[nlines];
+
+
+	//if file is unable to open, print error message
+	if (fptr == NULL) {
+		perror("my-cat: cannot open file\n");
+		exit(1);
+	}
+
+	
+
+//for each line in the file, store in a buffer, then print line
+	else{
+		
+		while (nlines > 0){
+			
+			char *next_line = fgets(lines, 100, fptr);
+			printf("%s", next_line);
+			nlines = nlines - 1;
+			
+		}
+
+
+	}
 //close file, clear processor resources
+	fclose(fptr);
+
+	return 0;
+}

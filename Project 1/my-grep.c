910111317e9a5f1003a22bd4c6476f5f5689aff6
nlamle <naalamle.boye@ashesi.edu.gh>
2018-09-19 00:16:12 +0000

    new functions
    new function myGrep which takes filename and search term as parameters

--- Project 1/my-grep.c
+++ Project 1/my-grep.c
@@ -11,20 +11,74 @@
 //close file, clear processor resources
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char search_term, char argv[])
+char *line;
+int myGrep(char *search_term, char *filename);
+
+
+int main(int argc, char *argv[])
 {
-	if (search_term == "")
+	if (argc == 1)
 	{
-		printf("No search term specified.\n");
+		printf("my-grep: searchterm[file..]\n");
+		exit(1);
 	}
-	
-	// FILE *grep_file = fopen(argv[0], "r");
-	// char curr_line;
 
-	// while(){
-	// 	curr_line = grep_file.getline();
+	//multiple files
+	int i = 1;
+	else{
+		while (i < argc){
+			myGrep(argv[1], argv[i+1]);
+			i = i + 1;
+		}
+	}
 
-	// }
+
+
 	return 0;
+	
+}
+
+
+int myGrep(char *search_term, char *filename){
+	FILE *fstream = fopen(filename, "r");
+
+		//if file is unable to open, print error message
+	if (fstream == NULL) {
+		perror("my-grep: cannot open file\n");
+		exit(1);
+	}
+
+	else{
+
+		//get number of lines in the file
+		int nlines = 0;
+		char chrs;
+		chrs = fgetc(fstream);
+
+		while (chrs != EOF){
+			if (chrs == '\n')
+			{
+				nlines = nlines + 1;
+			}
+
+			chrs = fgetc(fstream);
+		}
+		char *matching_line;
+
+		while (nlines > 0){
+
+			if (strstr(fstream, search_term)){
+				matching_line = strstr(fstream, search_term);
+				printf("%s\n", matching_line);
+			}
+
+			nlines = nlines - 1;
+
+		}
+
+		fclose(fstream);
+	}
 }

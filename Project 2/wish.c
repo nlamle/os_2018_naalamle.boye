1a8a5e18c4dabf3390a347e1e7ec7d1a7e88b8fd
nlamle <naalamle.boye@ashesi.edu.gh>
2018-09-25 22:59:19 +0000

    creating case for argument with a file, i.e., batch mode

--- Project 2/wish.c
+++ Project 2/wish.c
@@ -4,23 +4,53 @@
 
 int main(int argc, char const *argv[]) {
 
-  if(argc>2){
-   exit(0);
-  }
-
   char *input;
   size_t i_size = 64;
   size_t command;
-
   input = (char*)malloc(i_size * sizeof(char));
-  char* end = "exit";
 
-  while (input != end) {
-    printf("wish> " );
-    command = getline(&input, &i_size, stdin);
+  //if more than one argument is given, throw error and exit
+  if(argc>2)
+  {
+    perror("arguments must be one or none.");
+    exit(0);
   }
 
-  free(input);
 
+  //if no argument is given, run in interactive mode
+  else if(argc == 1)
+  {  
+    char* end = "exit";
+
+    while (input != end) {
+      printf("wish> " );
+      command = getline(&input, &i_size, stdin);
+    }
+
+  }
+
+
+  //if an argument (file name) is given, run in batch mode
+  else
+  {
+    FILE *wish_file = fopen(argv[1], "r");
+
+    if (wish_file == NULL)
+    {
+      perror("error opening file.");
+      exit(0);
+    }
+
+    while(!feof(wish_file))
+    {
+      command = getline(&input, &i_size, wish_file);
+    }
+
+  }
+
+
+
+
+  free(input);
   return 0;
 }

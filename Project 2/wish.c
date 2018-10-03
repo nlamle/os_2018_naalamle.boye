7662f7c5dea5511dd57744f7e69efb6f9225e8d8
nlamle <naalamle.boye@ashesi.edu.gh>
2018-10-03 01:33:02 +0000

    splitting input into tokens

--- Project 2/wish.c
+++ Project 2/wish.c
@@ -1,37 +1,50 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-void check_bltn(char *cmd);
+void check_bltn(char *cmd, char *arg);
+char *input, *bltn_cmd, *argmnt;
+size_t i_size = 64;
+size_t command;
+char *delimiters = "&\\/:;=- ";
+char cmd_args[100];
 
-
-int main(int argc, char const *argv[]) {
-
-  char *input;
-  size_t i_size = 64;
-  size_t command;
-  input = (char*)malloc(i_size * sizeof(char));
-
+int main(int argc, char const *argv[])
+{
+  input = (char *)malloc(i_size * sizeof(char));
   //if more than one argument is given, throw error and exit
-  if(argc>2)
+  if (argc > 2)
   {
     perror("An error has occurred.\n");
     exit(1);
   }
 
-
   //if no argument is given, run in interactive mode
-  else if(argc == 1)
-  {  
+  else if (argc == 1)
+  {
+    bltn_cmd = strtok(input, delimiters);
+    while (input != NULL)
+    {
 
-    while (input != NULL) {
-      check_bltn(input);
-      printf("wish> " );
+      for (argmnt = strtok(cmd_args, delimiters);
+           argmnt;
+           argmnt = strtok(NULL, delimiters))
+      {
+        printf("%s",argmnt);
+      }
+
+      check_bltn(bltn_cmd, argmnt);
+      printf("wish> ");
       command = getline(&input, &i_size, stdin);
+      // if (commands separated by &)
+      // {
+      //   /*run each command within the */
+      // }
     }
     exit(0);
   }
-
 
   //if an argument (file name) is given, run in batch mode
   else
@@ -44,7 +57,7 @@ int main(int argc, char const *argv[]) {
       exit(1);
     }
 
-    while(!feof(wish_file))
+    while (!feof(wish_file))
     {
       command = getline(&input, &i_size, wish_file);
     }
@@ -53,33 +66,39 @@ int main(int argc, char const *argv[]) {
     exit(0);
   }
 
-
-
-
   free(input);
   return 0;
 }
 
-void check_bltn(char *cmd){
+void check_bltn(char *cmd, char *arg)
+{
 
-  if (strcmp(cmd,"exit\n") == 0)
+  if (strcmp(cmd, "exit") == 0)
   {
-    exit(0);
+    if (arg == NULL)
+    {
+        exit(0);
+    }
+    else
+    {
+      perror("An error has occurred.\n");
+      exit(1);
+    }
   }
 
-  else if (strcmp(cmd, "cd\n") == 0)
+  else if (strstr(cmd, "cd") != NULL)
   {
-    // chdir(arg);
-    printf("hi\n");
+
+    chdir(arg);
   }
 
-  else if (strcmp(cmd, "path\n") == 0)
+  else if (strstr(cmd, "path") != NULL)
   {
     /* code */
   }
 
-  else{
+  else
+  {
     /* code */
   }
-
 }
